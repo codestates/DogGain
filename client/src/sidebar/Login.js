bc81ed3b13158066733f8cd3520b9aa79188b543
L9628 <mhp4718@gmail.com>
2022-05-15 21:58:59 +0900

    Login 구현중

--- client/src/sidebar/Login.js
+++ client/src/sidebar/Login.js
@@ -1,15 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Login () {
     const [loginInfo, setLoginInfo] = useState({
-        email: '',
+        username: '',
         password: '',
     })
     const [errMsg, setErrMsg] = useState('');
-    // const handleInputValue 
+    // const handleInputValue
+    const handleLogin = () => {
+        const { username, password } = loginInfo
+        if (!username || !password ) {
+            setErrMsg('username과 password를 입력해주세요.')
+        } else {
+            axios.post(
+                "https://localhost:3000/signin",
+                loginInfo,
+                { headers: { "Content-Type": "application/json" }, withCredentials: true }
+            )
+            .then((res) => {
+                handleResponseSuccess();
+            })
+        }
+    }
     return (
         <div>
-          
+            <div>
+                <input
+                    placeholder="username"
+                    type="string"
+                />
+            </div>
+            <div>
+                <input
+                    placeholder="password"
+                    type="string"
+                />
+            </div>
+            <div>
+                <button classname='login-btn' onClick={handleLogin}>
+                    로그인
+                </button>
+            </div>
+            <div>
+                <Link to='../pages/Signup'>아직 아이디가 없으신가요?</Link>
+            </div>
         </div>
     )
 }
