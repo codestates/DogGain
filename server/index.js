aa44e1642a2144d501666759bc426218fd34f6b3
woobaeh <qozkth@gmail.com>
2022-05-16 19:07:30 +0900

    modify index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 const express = require('express')
 const cors = require('cors')
 const userRouter = require('./router/user.js')
 const crawl = require('./controllers/deals')
-=======
-import express from 'express';
-import cors from 'cors';
-import userRouter from './router/user.js';
->>>>>>> 5138899 (connect local database)
 
 const app = express();
 
@@ -16,14 +10,10 @@ app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 
 
-<<<<<<< HEAD
 app.use('/user', userRouter)
 // setInterval(function() {
 //   console.log("hello!");
 // }, 3000);
 
-=======
-app.use('/user', userRouter);
->>>>>>> 5138899 (connect local database)
 
 app.listen(80);
