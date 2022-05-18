763268173a1544373e33aba3134aec7618370696
lasags <lasags@naver.com>
2022-05-19 03:03:25 +0900

    crawl component 구현중

--- client/src/main/Main.js
+++ client/src/main/Main.js
@@ -1,12 +1,22 @@
 import Top3ranking from './Top3ranking';
 import Crawling from './Crawling';
 import deals from '../data/Deals';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Main() {
+  let [hotdeals, setHotDeals] = useState('');
+  // useEffect(() => {
+  //   axios
+  //     .get('http://localhost:8080/deal?&page=3&pageSige=5')
+  //     .then((rp) =>
+  //     console.log(rp.data)
+  //     setHotDeals(rp.data));
+  // }, []);
   return (
     <div>
-      <Top3ranking deals={deals} />
-      <Crawling deals={deals} />
+      <Top3ranking hotdeals={hotdeals} />
+      <Crawling hotdeals={hotdeals} />
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,21 +7,21 @@ import Footer from '../Footer';
 function Home() {
   return (
     <div>
-      <div className="head">
+      <div className='head'>
         <Header />
       </div>
       <div>
         <Nav />
       </div>
-      <div className="content">
-        <div className="sidebar">
+      <div className='content'>
+        <div className='sidebar'>
           <Sidebar />
         </div>
-        <div className="main">
+        <div className='main'>
           <Main />
         </div>
       </div>
-      <div className="home-foot">
+      <div className='home-foot'>
         <Footer />
       </div>
     </div>
