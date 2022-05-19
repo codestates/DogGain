84e49a41f310e2336c34c49d3edf3baa3e1acc1c
lasags <lasags@naver.com>
2022-05-20 01:02:59 +0900

    client_real_final

--- server/router/deal.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const dealController = require('../controllers/dealController')
-
-router.get("/", dealController.getDeal);
-router.post("/", dealController.postComment);
-
-module.exports = router;
