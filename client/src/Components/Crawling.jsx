2ecb8073c541589a71a5e27e0a1fbe3eb5f9d63a
lasags <lasags@naver.com>
2022-09-21 12:23:00 +0900

    fix Crawling.jsx css

--- client/src/Components/Crawling.jsx
+++ client/src/Components/Crawling.jsx
@@ -9,10 +9,18 @@ function Crawling({ hotdeals }) {
             <div className='bg-white p-4 mb-4' key={deal.id}>
               <div style={{ flex: 0.1 }}></div>
               <div className='dofinal' style={{ flex: 8 }}>
-                <div className='tag-list'>
-                  <div className='shopname_tag'>{deal.origin_sitename}</div>
+                <div className='flex items-center text-2xs'>
+                  <div className='shopname_tag display: inline-block rounded-md bg-[color:#a4cff5] my-1 p-1 '>{deal.origin_sitename}</div>
+
+                  <div id='origin_user' className='flex ml-2 mt-1 text-[color:#777777]'>
+                  <FaUserAlt className='' /> 
+                  <div className=''>
+                  {deal.origin_username}
+                  </div>
                 </div>
-                <a href={deal.origin_url} className='ranking_alink'>
+
+                </div>
+                <a href={deal.origin_url} id='ranking_alink' className=' text-lg text-blue-50 text-ellipsis overflow-hidden max-w-3xl mt-2 text-decoration-line:none' >
                   {deal.origin_title}
                 </a>
                 {deal.origin_price === 0 ? (
@@ -20,9 +28,6 @@ function Crawling({ hotdeals }) {
                 ) : (
                   <div style={{ color: 'tomato' }}>{deal.origin_price}원</div>
                 )}
-                <div className='origin_user'>
-                  <FaUserAlt /> {deal.origin_username}
-                </div>
               </div>
             </div>
           );
