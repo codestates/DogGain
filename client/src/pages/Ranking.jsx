18de141f360dad1b29dc5a686176cf4c7fa5c588
lasags <lasags@naver.com>
2022-09-27 08:49:02 +0900

    REMOVE Ranking.jsx/remark

--- client/src/pages/Ranking.jsx
+++ client/src/pages/Ranking.jsx
@@ -17,20 +17,6 @@ function Ranking() {
     setDealsRanking(deal => dealsFilter(deals))
   },[deals])
 
-  // {hotdeals.slice()
-  //   .sort((a, b) => b.origin_views - a.origin_views)
-  //   .filter((deal, idx) => (idx <= 2 ? deal : undefined))
-  //   .map((deal, idx) => {
-  //     return (
-  //       <div id="hotdeals_container" className="truncate max-w-xl text-xs font-semibold text-blue-50 mb-1" key={deal.id}>
-  //         <span id="ranking_label" className="text-[color:#90949c] mr-2 ">{idx + 1}위</span>
-  //         <a href={deal.origin_url} target="_blank" rel="noreferrer" id="ranking_alink" className=" max-w-xl">
-  //           {deal.origin_title}
-  //         </a>
-  //       </div>
-  //     );
-  //   })}
-
   return (
     <div className="mt-4">
           <Crawling hotdeals={dealsRanking} />
